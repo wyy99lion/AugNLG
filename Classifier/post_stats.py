6687b7f052178ebed4eab56fc165fb19d0502edb
wyy99lion <96676591+wyy99lion@users.noreply.github.com>
2022-07-07 11:28:15 +0800

    Update post_stats.py

--- Classifier/post_stats.py
+++ Classifier/post_stats.py
@@ -14,20 +14,39 @@ def str2bool(v):
 
 
 def n_grams(tokens, n):
+    # tuple是另一种有序的列表，也称为“ 元组 ”。tuple 和 list 非常类似，但是，tuple一旦创建完毕，就不能修改了。
+    # 获得tokens中的ngram并返回
     l = len(tokens)
     return [tuple(tokens[i:i + n]) for i in range(l) if i + n < l]
 
+
 def has_repeat(elements):
+    # 如果元素存在重复，返回true
     d = set(elements)
+    # set() 函数创建一个无序不重复元素集，可进行关系测试，删除重复数据，还可以计算交集、差集、并集等。
     return len(d) < len(elements)
 
 def cal_self_repeat(summary):
     ngram_repeats = {2: 0, 4: 0, 8: 0}
     sents = summary.split('<q>')
     for n in ngram_repeats.keys():
+        # keys函数是Python的字典函数，它返回字典中的所有键所组成的一个可迭代序列。使用keys()可以获得字典中所有的键。
+        # 即，for n in [2,4,8]
         # Respect sentence boundary
         grams = reduce(lambda x, y: x + y, [n_grams(sent.split(), n) for sent in sents], [])
+        '''
+        获得 sents 中每个sent中的ngram和n，将其相加后返回
+        
+        reduce(function,iterable,initial)
+        第一个参数是函数function，reduce()只能接受一个带有两个参数的函数；
+        第二个参数是iterable，即可迭代对象，可以是列表、字符串等序列；
+        第三个参数为初始值，可选可不选，但前两个参数是必须的；
+        reduce()的用法：
+        reduce()函数将一个序列内的所有元素按照序列顺序依次传入func函数中，并将得到的值继续作为参数与下一个序列中的元素进行操作，一直重复到序列中无元素为止；
+        注意到reduce()的参数函数有两个参数了吧，如果没有指定初始值的话，那么传入函数的就是序列的前两个值，如果指定了初始值，那么传入的就是初始值和序列的第一个值
+        '''
         ngram_repeats[n] += has_repeat(grams)
+        #计算每一组ngram和n的组合有没有重复
     return ngram_repeats
 
 def cal_novel(summary, gold, source, summary_ngram_novel, gold_ngram_novel):
