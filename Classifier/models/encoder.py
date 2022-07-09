6e1d3562e134a548ab96ed1f01085b7c48aba2dd
wyy99lion <96676591+wyy99lion@users.noreply.github.com>
2022-07-09 17:12:23 +0800

    Update encoder.py

--- Classifier/models/encoder.py
+++ Classifier/models/encoder.py
@@ -22,7 +22,7 @@ class Classifier(nn.Module):
 class PositionalEncoding(nn.Module):
 
     def __init__(self, dropout, dim, max_len=5000):
-        pe = torch.zeros(max_len, dim)
+        pe = torch.zeros(max_len, dim) #pe（最长长度*维度）
         position = torch.arange(0, max_len).unsqueeze(1)
         div_term = torch.exp((torch.arange(0, dim, 2, dtype=torch.float) * -(math.log(10000.0) / dim)))
         pe[:, 0::2] = torch.sin(position.float() * div_term)
@@ -30,11 +30,13 @@ class PositionalEncoding(nn.Module):
         pe = pe.unsqueeze(0)
         super(PositionalEncoding, self).__init__()
         self.register_buffer('pe', pe)
+        # self.register_buffer(‘name’,Tensor)，该方法的作用在于定义一组参数。
+        # 该组参数在模型训练时不会更新（即调用optimizer.step()后该组参数不会变化，只可人为地改变它们的值），但是该组参数又作为模型参数不可或缺的一部分。
         self.dropout = nn.Dropout(p=dropout)
         self.dim = dim
 
     def forward(self, emb, step=None):
-        emb = emb * math.sqrt(self.dim)
+        emb = emb * math.sqrt(self.dim) 
         if (step):
             emb = emb + self.pe[:, step][:, None, :]
 
@@ -55,6 +57,12 @@ class TransformerEncoderLayer(nn.Module):
             heads, d_model, dropout=dropout)
         self.feed_forward = PositionwiseFeedForward(d_model, d_ff, dropout)
         self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
+        # nn.LayerNorm(normalized_shape, eps=1e-05, elementwise_affine=True, device=None, dtype=None)
+        # 对每单个batch进行的归一化。
+        # normalized_shape：归一化的维度，int（最后一维）list（list里面的维度）
+        # eps：加在方差上的数字，避免分母为0
+        # elementwise_affine：bool，True的话会有一个默认的affine参数
+        
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, iter, query, inputs, mask):
@@ -79,6 +87,7 @@ class ExtTransformerEncoder(nn.Module):
         self.transformer_inter = nn.ModuleList(
             [TransformerEncoderLayer(d_model, heads, d_ff, dropout)
              for _ in range(num_inter_layers)])
+        # 创建num_inter_layers个TransformerEncoderLayer层
         self.dropout = nn.Dropout(dropout)
         self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
         self.wo = nn.Linear(d_model, 1, bias=True)
@@ -88,6 +97,7 @@ class ExtTransformerEncoder(nn.Module):
         """ See :obj:`EncoderBase.forward()`"""
 
         batch_size, n_sents = top_vecs.size(0), top_vecs.size(1)
+        #分别为第0维和第1维的数据
         pos_emb = self.pos_emb.pe[:, :n_sents]
         x = top_vecs * mask[:, :, None].float()
         x = x + pos_emb
