1fe79f84f3178e1a25630e89c417b2bbce87fdf6
wyy99lion <96676591+wyy99lion@users.noreply.github.com>
2022-07-11 17:01:18 +0800

    Update logging.py

--- Classifier/others/logging.py
+++ Classifier/others/logging.py
@@ -8,10 +8,13 @@ logger = logging.getLogger()
 
 def init_logger(log_file=None, log_file_level=logging.NOTSET):
     log_format = logging.Formatter("[%(asctime)s %(levelname)s] %(message)s")
+    #易读的时间格式、消息的级别名称(‘DEBUG’, ‘INFO’, ‘WARNING’, ‘ERROR’, ‘CRITICAL’)、记录的信息
     logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
+    logger.setLevel(logging.INFO) ## Log等级总开关
 
     console_handler = logging.StreamHandler()
+    #使用这个Handler可以向类似与sys.stdout或者sys.stderr的任何文件对象(file object)输出信息。
+    #它的构造函数是： StreamHandler([strm])其中strm参数是一个文件对象。默认是sys.stderr
     console_handler.setFormatter(log_format)
     logger.handlers = [console_handler]
 
