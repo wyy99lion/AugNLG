7aeebb00430ca5b6d727bebe25a831bff0d51b9c
wyy99lion <96676591+wyy99lion@users.noreply.github.com>
2022-07-11 17:28:04 +0800

    Update reporter_ext.py

--- Classifier/models/reporter_ext.py
+++ Classifier/models/reporter_ext.py
@@ -10,14 +10,18 @@ from others.logging import logger
 
 def build_report_manager(opt):
     if opt.tensorboard:
-        from tensorboardX import SummaryWriter
+        from tensorboardX import SummaryWriter 
+        ## SummaryWriter压缩（包括）了所有内容
         tensorboard_log_dir = opt.tensorboard_log_dir
 
         if not opt.train_from:
-            tensorboard_log_dir += datetime.now().strftime("/%b-%d_%H-%M-%S")
+            tensorboard_log_dir += datetime.now().strftime("/%b-%d_%H-%M-%S") #记录时间
 
         writer = SummaryWriter(tensorboard_log_dir,
                                comment="Unmt")
+        ##创建writer object，log会被存入tensorboard_log_dir，后缀为"Unmt"
+        #eg tensorboard_log_dir为'logging-1',则存入的文件为'logging-1-Unmt'
+        
     else:
         writer = None
 
@@ -28,8 +32,8 @@ def build_report_manager(opt):
 
 class ReportMgrBase(object):
     """
-    Report Manager Base class
-    Inherited classes should override:
+    Report Manager Base class  Report管理器基类
+    Inherited classes should override继承的类应该覆盖：:
         * `_report_training`
         * `_report_step`
     """
@@ -38,6 +42,8 @@ class ReportMgrBase(object):
         """
         Args:
             report_every(int): Report status every this many sentences
+            report_every(int)：每隔这么多句子报告状态
+            start_time(float)：手动设置报表开始时间。 负值意味着您需要稍后设置或使用`start()`
             start_time(float): manually set report start time. Negative values
                 means that you will need to set it later or use `start()`
         """
@@ -56,12 +62,13 @@ class ReportMgrBase(object):
         """
         This is the user-defined batch-level traing progress
         report function.
+        这是用户自定义的批次级训练进度报告功能。
 
         Args:
             step(int): current step count.
             num_steps(int): total number of batches.
             learning_rate(float): current learning rate.
-            report_stats(Statistics): old Statistics instance.
+            report_stats(Statistics): old Statistics instance.旧的统计实例。
         Returns:
             report_stats(Statistics): updated Statistics instance.
         """
@@ -81,12 +88,12 @@ class ReportMgrBase(object):
             return report_stats
 
     def _report_training(self, *args, **kwargs):
-        """ To be overridden """
+        """ To be overridden要被覆盖 """
         raise NotImplementedError()
 
     def report_step(self, lr, step, train_stats=None, valid_stats=None):
         """
-        Report stats of a step
+        Report stats of a step报告步骤的统计信息
 
         Args:
             train_stats(Statistics): training stats
@@ -105,11 +112,12 @@ class ReportMgr(ReportMgrBase):
         """
         A report manager that writes statistics on standard output as well as
         (optionally) TensorBoard
+        一个报告管理器，它在标准输出以及（可选）TensorBoard 上编写统计信息
 
         Args:
-            report_every(int): Report status every this many sentences
+            report_every(int): Report status every this many sentences每隔多少句子报告状态
             tensorboard_writer(:obj:`tensorboard.SummaryWriter`):
-                The TensorBoard Summary writer to use or None
+                The TensorBoard Summary writer to use or None要使用的 TensorBoard 摘要编写器或无
         """
         super(ReportMgr, self).__init__(report_every, start_time)
         self.tensorboard_writer = tensorboard_writer
@@ -126,8 +134,10 @@ class ReportMgr(ReportMgrBase):
         """
         report_stats.output(step, num_steps,
                             learning_rate, self.start_time)
+        #输出step, num_steps,learning_rate, self.start_time
 
         # Log the progress using the number of batches on the x-axis.
+        # 使用 x 轴上的批次数记录进度。
         self.maybe_log_tensorboard(report_stats,
                                    "progress",
                                    learning_rate,
@@ -159,7 +169,7 @@ class ReportMgr(ReportMgrBase):
 
 class Statistics(object):
     """
-    Accumulator for loss statistics.
+    Accumulator for loss statistics.用于损失统计的累加器
     Currently calculates:
 
     * accuracy
@@ -175,15 +185,16 @@ class Statistics(object):
     @staticmethod
     def all_gather_stats(stat, max_size=4096):
         """
-        Gather a `Statistics` object accross multiple process/nodes
+        Gather a `Statistics` object accross multiple process/nodes跨多个进程/节点收集“统计”对象
 
         Args:
             stat(:obj:Statistics): the statistics object to gather
                 accross all processes/nodes
-            max_size(int): max buffer size to use
+                收集所有进程/节点的统计信息对象
+            max_size(int): max buffer size to use要使用的最大缓冲区大小
 
         Returns:
-            `Statistics`, the update stats object
+            `Statistics`, the update stats object更新统计对象
         """
         stats = Statistics.all_gather_stats_list([stat], max_size=max_size)
         return stats[0]
@@ -192,10 +203,12 @@ class Statistics(object):
     def all_gather_stats_list(stat_list, max_size=4096):
         """
         Gather a `Statistics` list accross all processes/nodes
+        收集所有进程/节点的“统计”列表
 
         Args:
             stat_list(list([`Statistics`])): list of statistics objects to
                 gather accross all processes/nodes
+                要在所有进程/节点中收集的统计对象列表
             max_size(int): max buffer size to use
 
         Returns:
@@ -205,6 +218,7 @@ class Statistics(object):
         from distributed import all_gather_list
 
         # Get a list of world_size lists with len(stat_list) Statistics objects
+        # 使用 len(stat_list) 统计对象获取 world_size 列表的列表
         all_stats = all_gather_list(stat_list, max_size=max_size)
 
         our_rank = get_rank()
@@ -219,6 +233,7 @@ class Statistics(object):
     def update(self, stat, update_n_src_words=False):
         """
         Update statistics by suming values with another `Statistics` object
+        通过将值与另一个 `Statistics` 对象相加来更新统计信息
 
         Args:
             stat: another statistic object
@@ -237,11 +252,11 @@ class Statistics(object):
         return self.loss / self.n_docs
 
     def elapsed_time(self):
-        """ compute elapsed time """
+        """ compute elapsed time 计算经过的时间 """
         return time.time() - self.start_time
 
     def output(self, step, num_steps, learning_rate, start):
-        """Write out statistics to stdout.
+        """Write out statistics to stdout.将统计信息写入标准输出
 
         Args:
            step (int): current step
