2c25d2c49dc99a8fbf3fb889d0c0b5039cf6c857
wyy99lion <96676591+wyy99lion@users.noreply.github.com>
2022-07-14 12:33:49 +0800

    Update key_ngrams注释.py

--- key_ngrams注释.py
+++ key_ngrams注释.py
@@ -29,13 +29,14 @@ def preprocess(string, replace_num=True, tokenization=True):
                 number_length_dict[len(item)] = []
             number_length_dict[len(item)].append(item)
         for num_len in sorted(number_length_dict.items(), key = lambda d:d[0], reverse = True):
-            for number in num_len[1]:
+            # .items()以列表返回可遍历的(键, 值) 元组数组。然后按数字长度排序
+            for number in num_len[1]:#返回每个值
                 string = string.replace(number, '[NUMBER]')
     # replace is for tfidf tool
     return string.replace('[NUMBER]', '11111')
 
 def ngram_validation_check(ngram):
-    # ngram 验证检查。如果ngram关键词为空，返回false
+    # ngram 验证检查。如果ngram关键词任意一个为空或长度为1，返回false
     for tok in ngram:
         if len(tok) <= 1:
             return False
@@ -48,9 +49,9 @@ def get_ngrams(string, n):
     for i in range(0, len(flist)-n+1):
         if not ngram_validation_check(flist[i:i+n]):
             continue
-            #如果i对应的ngram为空，即其中没有词，跳出循环
+            #如果i对应的ngram为空或1，即其中词长度<=1，跳出循环
         ngram = ' '.join(flist[i:i+n]) #将i至i+n个词定义为i对应的ngram
-        ngrams.append(ngram) #将符合条件的ngram添加到ngrams
+        ngrams.append(ngram) #将符合条件的ngram添加到ngrams,最后一个ngram长度可能为2
     return ngrams
 
 def stopword_filtered(stopwords, key):
@@ -108,13 +109,15 @@ if __name__ == '__main__':
     in_domain_path = in_domain_path.replace('[DOMAIN]', args.domain)
     for line in open(in_domain_path):
         flist = line.strip().split(' & ')  #去除头尾空字符后，用&分割得到的字符串列表
+        #样例 inform_no_match ( pricerange = pricey ; acceptscreditcards = no ) 
+            #  & there are no hotel -s in pricey price range that do not accept card -s
         #strip()方法用于移除字符串头尾指定的字符(默认为空格)。
-        #.split(' & ') 使用分隔符' & '分割
+        #.split(' & ') 使用分隔符' & '分割,前面为mr,后面为句子
         if len(flist) != 2:
             continue
-            #如果其line中字符串数量（句子数量）少于2，跳出循环
-        utterance = flist[1].strip() #选取该行第二个句子，去除首位字符
-        in_domain_exps.append(utterance) #将所有行的第二个句子添加到in_domain_exps
+            #如果其line中字符串数量（句子数量）不为2，跳出循环
+        utterance = flist[1].strip() #选取该行第二个句子，即生成的句子，去除首位字符
+        in_domain_exps.append(utterance) #将所有句子添加到in_domain_exps
 
 
     # In-domain examples域内例子
@@ -128,6 +131,7 @@ if __name__ == '__main__':
         #获得每个域内例句的ngram构成（ngrams），组合成为in_domain_ngrams
     in_count = Counter(in_domain_ngrams)
     #对字符串\列表\元祖\字典（域内ngram关键词）进行计数,返回一个字典类型的数据,键是元素,值是元素出现的次数
+    #每个ngram出现的次数
 
 
     # Out-domain utterances
@@ -143,6 +147,7 @@ if __name__ == '__main__':
         vocabulary[key] = len(vocabulary) #为每个关键词赋予编号，获得每个ngram关键词为键，对应编号为值的字典
     vectorizer = TfidfVectorizer(vocabulary=vocabulary, smooth_idf=True, use_idf=True, ngram_range=(2, 2))
     '''
+    计算每个句子中，每个单词的值。行为句子，列为单词对应的值
     TfidfVectorizer： 除了考量某词汇在文本出现的频率，还关注包含这个词汇的所有文本的数量。
     能够削减高频没有意义的词汇出现带来的影响, 挖掘更有意义的特征。属于Tfidf特征。
     TF为某个词在文章中出现的总次数。为了消除不同文章大小之间的差异，便于不同文章之间的比较，我们在此标准化词频：TF = 某个词在文章中出现的总次数/文章的总词数。
@@ -151,7 +156,8 @@ if __name__ == '__main__':
     TF-IDF值 = TF * IDF。
     字典为vocabulary
     ngram_range：例如ngram_range(min,max)，是指将text分成min，min+1，min+2,.........max 个不同的词组。
-            比如 '我 爱 中国' 中ngram_range(1,3)之后可得到'我'  '爱'  '中国'  '我 爱'  '爱 中国' 和'我 爱 中国'，如果是ngram_range (1,1) 则只能得到单个单词'我'  '爱'和'中国'。
+            比如 '我 爱 中国' 中ngram_range(1,3)之后可得到'我'  '爱'  '中国'  '我 爱'  '爱 中国' 和'我 爱 中国'，
+            如果是ngram_range (1,1) 则只能得到单个单词'我'  '爱'和'中国'。
 
     '''
     #vectorizer = TfidfVectorizer(smooth_idf=True, use_idf=True, ngram_range=(2, 2))
